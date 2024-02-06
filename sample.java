1036dd8601778a7c02b9b8b515bf003efe8ce543
chrisance2002 <chrisance2002@gmail>
2024-02-06 11:51:16 +0530

    first

---
+++ sample.java
@@ -0,0 +1,12 @@
+public class sample 
+{
+    public static void main(String[] args)
+    {
+        int a=4;
+        int b=2;
+        int x= a+b;
+        int y= a-b;
+        System.out.println("Sum: "+x);
+        System.out.println("Diff: "+x);
+    }    
+}
