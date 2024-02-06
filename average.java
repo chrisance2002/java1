78f77b28eabeb9221561ce1d6456472644375d14
chrisance2002 <chrisance2002@gmail>
2024-02-06 12:08:10 +0530

    average

---
+++ average.java
@@ -0,0 +1,13 @@
+public class average 
+{
+    public static void main(String[] args)
+    {
+        int a=2;
+        int b=4;
+        int c=6;
+        int d=a+b+c;
+        int e=d/3;
+        System.out.println("Sum of three numbers: "+d);
+        System.out.println("Average: "+e);
+    }   
+}
